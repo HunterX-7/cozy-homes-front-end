9d21167821789038c50fefc969d984646f8f2ef0
HunterX-7 <fraquin7@gmail.com>
2023-05-13 23:13:12 -0500

    Configure the redux store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import houseSlice from "./houseSlice";
+
+const rootReducer = {
+    house: houseSlice,
+  };
+  
+  const store = configureStore({ reducer: rootReducer });
+  
+  export default store;
