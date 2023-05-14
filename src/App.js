ac0d85e057373254a290b4492fccf5df6d50bdb7
HunterX-7 <fraquin7@gmail.com>
2023-05-13 23:12:49 -0500

    Add routes and update the app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
-}
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Houses from './components/Houses';
+
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Houses />} />
+  </Routes>
+);
 
 export default App;
