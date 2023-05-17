92da99984714c3d156fd7e082d49a357731f8a70
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-05-16 19:49:43 -0600

    Add case house slice delete

--- src/redux/houseSlice.js
+++ src/redux/houseSlice.js
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const FETCH_HOUSE = 'Cozy-homes-front-end/houses/FETCH_HOUSES';
 const ADD_HOUSE = 'Cozy-homes-front-end/houses/ADD_HOUSE';
+const DELETE_HOUSE = 'Cozy-homes-front-end/houses/DELETE_HOUSE';
 
 export const fetchHouses = createAsyncThunk(FETCH_HOUSE, async () => {
   const response = await fetch('http://localhost:3000/api/v1/houses');
@@ -24,6 +25,19 @@ export const addHouse = createAsyncThunk(ADD_HOUSE, async (house) => {
   return data;
 });
 
+export const deleteHouse = createAsyncThunk(
+  DELETE_HOUSE,
+  async (id) => {
+    await fetch(`http://localhost:3000/api/v1/houses/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return id;
+  },
+);
+
 const houseSlice = createSlice({
   name: 'house',
   initialState: {
@@ -37,6 +51,10 @@ const houseSlice = createSlice({
       })
       .addCase(addHouse.fulfilled, (state, action) => {
         state.property.push(action.payload);
+      })
+      .addCase(deleteHouse.fulfilled, (state, action) => {
+        const id = action.payload;
+        state.property = state.property.filter((house) => house.id !== id);
       });
   },
 });
