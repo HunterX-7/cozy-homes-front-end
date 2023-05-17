c4764d4fc23703489e458adf917360c467347374
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-05-16 19:47:22 -0600

    update add functionality Remove house

--- src/components/RemoveHouse.js
+++ src/components/RemoveHouse.js
@@ -1,6 +1,28 @@
-const RemoveHouse = () => (
-  <h1>hello</h1>
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchHouses } from '../redux/houseSlice';
+import RemoveHouseList from './RemoveHouseList';
+import NavBar from './NavBar';
 
-);
+const RemoveHouse = () => {
+  const houses = useSelector((state) => state.house.property);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchHouses());
+  }, [dispatch]);
+
+  return (
+
+    <div>
+      <NavBar />
+      <div>
+        <RemoveHouseList
+          data={houses}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default RemoveHouse;
