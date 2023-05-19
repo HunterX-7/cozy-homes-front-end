338d59b4adfaea998ef3e3ea578f63fd23da76c5
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-05-19 17:19:33 -0600

    Add functionaliti nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,34 @@
 import { Link } from 'react-router-dom';
+import { func } from 'prop-types';
 
-const Navbar = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Houses</Link>
-      </li>
-      <li>
-        <Link to="/reserve">Reserve</Link>
-      </li>
-      <li>
-        <Link to="/add">AddHouse</Link>
-      </li>
-      <li>
-        <Link to="/remove">removeHouse</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const Navbar = (props) => {
+  const { handleLogout } = props;
 
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/home">Houses</Link>
+        </li>
+        <li>
+          <Link to="/reserve">Reserve</Link>
+        </li>
+        <li>
+          <Link to="/reservations">Reservations</Link>
+        </li>
+        <li>
+          <Link to="/add">AddHouse</Link>
+        </li>
+        <li>
+          <Link to="/remove">removeHouse</Link>
+        </li>
+      </ul>
+      <button type="button" onClick={handleLogout} className="">log_out</button>
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  handleLogout: func.isRequired,
+};
 export default Navbar;
