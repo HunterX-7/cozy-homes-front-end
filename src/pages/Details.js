70869f96953ddf8294aa1f26c739ab1563f21515
gabicilfone <cilfonegabriel@gmail.com>
2023-05-19 19:11:23 -0300

    Add details for houses

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,48 +1,45 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import NavBar from '../components/NavBar';
 
 const Details = () => {
   const { name } = useParams(); // se usa para setear la busqueda por nombres en detalles
   const house = useSelector((state) => state.house.property.find((hous) => hous.name === name));
 
   return (
-    <div>
-      <NavBar />
-      <div className="detailscontainer">
-        <h1>{house.name}</h1>
-        <ul>
-          <li><img src={house.photo} alt="house_pic" /></li>
-          <li>{house.description}</li>
-          <li>
-            Type:
-            {house.house_type}
-          </li>
-          <li>
-            Garages:
-            {house.garage}
-          </li>
-          <li>
-            Bedrooms:
-            {house.bedrooms}
-          </li>
-          <li>
-            Bathrooms:
-            {house.bathrooms}
-          </li>
-          <li>
-            Area m2:
-            {house.area}
-          </li>
-          <li>
-            Price:
-            {house.daily_price}
-          </li>
 
-        </ul>
+    <div className="detailscontainer">
+      <h1>{house.name}</h1>
+      <ul>
+        <li><img src={house.photo} alt="house_pic" /></li>
+        <li>{house.description}</li>
+        <li>
+          Type:
+          {house.house_type}
+        </li>
+        <li>
+          Garages:
+          {house.garage}
+        </li>
+        <li>
+          Bedrooms:
+          {house.bedrooms}
+        </li>
+        <li>
+          Bathrooms:
+          {house.bathrooms}
+        </li>
+        <li>
+          Area m2:
+          {house.area}
+        </li>
+        <li>
+          Price:
+          {house.daily_price}
+        </li>
 
-      </div>
+      </ul>
+
     </div>
   );
 };
