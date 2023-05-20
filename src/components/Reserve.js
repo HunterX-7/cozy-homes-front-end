f3bede62d6c8593e0a58ffb2279859b0a8968978
gabicilfone <cilfonegabriel@gmail.com>
2023-05-20 16:48:56 -0300

    Add DatePicker for reserve

--- src/components/Reserve.js
+++ src/components/Reserve.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router';
 import { func } from 'prop-types';
+import DatePicker from 'react-datepicker';
 import Selection from './Selection';
-
+import 'react-datepicker/dist/react-datepicker.css';
 import { fetchHouses } from '../redux/houseSlice';
+import './stylesheets/reserve.css';
 
 const AddReservations = (props) => {
   const { SessionStatus } = props;
@@ -50,8 +52,8 @@ const AddReservations = (props) => {
     const Reservation = {
       user_id: user.id,
       house_id: house.id,
-      startDate,
-      endDate,
+      startDate: startDate.toISOString(),
+      endDate: endDate.toISOString(),
     };
 
     const response = await fetch('http://localhost:3000/api/v1/reservations/', {
@@ -74,22 +76,23 @@ const AddReservations = (props) => {
   };
 
   return (
-    <div className="">
-      <div className="">
-        <div className="">
-          <h3 className="">RESERVE A HOUSE</h3>
+    <div className="container">
+      <div className="content">
+        <div className="header">
+          <h3>RESERVE A HOUSE</h3>
           <span className="line" />
-          <p className="">
-            Discover the epitome of luxury living with our exquisite rental homes.
-            Experience a world of opulence and refinement, where every detail has been
-            meticulously designed to exceed your expectations. From breathtaking
-            architecture to lavish amenities, our luxury houses offer a lifestyle of
-            unparalleled elegance.
-          </p>
-          <form className="form_container" onSubmit={handleSubmit}>
-            <select id="houses" name="houses" className="select_house mt-3" onChange={handleHouseChange} required>
-              <option value="">Select A House</option>
-              {
+        </div>
+        <p className="description">
+          Discover the epitome of luxury living with our exquisite rental homes.
+          Experience a world of opulence and refinement, where every detail has been
+          meticulously designed to exceed your expectations. From breathtaking
+          architecture to lavish amenities, our luxury houses offer a lifestyle of
+          unparalleled elegance.
+        </p>
+        <form className="form-container" onSubmit={handleSubmit}>
+          <select id="houses" name="houses" className="select_house mt-3" onChange={handleHouseChange} required>
+            <option value="">Select A House</option>
+            {
                 houses.length > 0 ? (
                   houses.map((house) => (
                     <Selection
@@ -102,32 +105,28 @@ const AddReservations = (props) => {
                   <option value="no house">No houses avaliable</option>
                 )
               }
-            </select>
+          </select>
+          <DatePicker
+            className=""
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            placeholderText="start date: dd/mm/yyyy"
+            dateFormat="dd/MM/yyyy"
+            required
+          />
 
-            <input
-              className=""
-              required
-              type="text"
-              value={startDate}
-              onChange={(e) => setStartDate(e.target.value)}
-              placeholder="start date: dd/mm/yyyy"
-
-            />
-            <input
-              className=""
-              required
-              type="text"
-              value={endDate}
-              onChange={(e) => setEndDate(e.target.value)}
-              placeholder="end date: dd/mm/yyyy"
-
-            />
-
-            <button type="submit" className="mt-3">Reserve Now</button>
-          </form>
-          <div className="mt-5 status-notification">
-            {status}
-          </div>
+          <DatePicker
+            className=""
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+            placeholderText="end date: dd/mm/yyyy"
+            dateFormat="dd/MM/yyyy"
+            required
+          />
+          <button type="submit" className="mt-3">Reserve Now</button>
+        </form>
+        <div className="status-notification">
+          {status}
         </div>
       </div>
     </div>
