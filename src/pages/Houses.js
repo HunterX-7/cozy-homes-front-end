873761525d428db06b6c08bf35d9d8efa7d4f030
HunterX-7 <fraquin7@gmail.com>
2023-05-22 18:15:36 -0500

    Add the Housecarousel and style the main page

--- src/pages/Houses.js
+++ src/pages/Houses.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router';
 import { func } from 'prop-types';
 import { fetchHouses } from '../redux/houseSlice';
-import HousesList from '../components/HousesList';
+import HouseCarousel from '../components/HouseCarrusel';
 
 const Houses = (props) => {
   const { SessionStatus } = props;
@@ -23,14 +23,26 @@ const Houses = (props) => {
   }, [dispatch, SessionStatus, navigate]);
 
   return (
-
-    <div>
-
-      <HousesList
-        HousesData={houses}
-      />
-    </div>
-
+    <>
+      <div className="d-none d-lg-block col-lg-8 p-0">
+        <div className="col-12">
+          <div className="box" />
+        </div>
+        <h2 className="text-center">LATEST PROPERTIES</h2>
+        <p className="text-center">Please select a property</p>
+        <hr className="new1 mx-auto" />
+        <HouseCarousel HousesData={houses} />
+      </div>
+      <div className="d-lg-none p-5">
+        <div className="col-12">
+          <div className="box" />
+        </div>
+        <h2 className="text-center">LATEST PROPERTIES</h2>
+        <p className="text-center">Please select a property</p>
+        <hr className="new1 mx-auto" />
+        <HouseCarousel HousesData={houses} />
+      </div>
+    </>
   );
 };
 
