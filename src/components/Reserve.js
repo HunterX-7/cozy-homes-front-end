cdd16139f893e95845f25fa11305dfc24fa92b51
HunterX-7 <fraquin7@gmail.com>
2023-05-22 20:01:46 -0500

    Add styling to the reservation page and bg-photo

--- src/components/Reserve.js
+++ src/components/Reserve.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router';
 import { func } from 'prop-types';
+import { Link } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
+import { AiFillHome } from 'react-icons/ai';
 import Selection from './Selection';
 import 'react-datepicker/dist/react-datepicker.css';
 import { fetchHouses } from '../redux/houseSlice';
@@ -77,62 +79,72 @@ const AddReservations = (props) => {
   };
 
   return (
-    <div className="contenedor col-lg-8 p-0">
-      <div className="content">
-        <div className="header">
-          <h3>RESERVE A HOUSE</h3>
-          <span className="line" />
-        </div>
-        <p className="description">
-          Embark on a journey of unrivaled luxury as you step into
-          our extraordinary collection of rental homes.
-          Prepare to be captivated by a world of refinement and elegance,
-          where every aspect has been thoughtfully curated to
-          surpass even the loftiest of expectations.
-          Immerse yourself in awe-inspiring architectural masterpieces
-          and indulge in lavish amenities, as our opulent residences
-          offer a lifestyle that epitomizes pure extravagance.
-          Experience a new standard of sophistication and elevate your
-          living to extraordinary heights.
-        </p>
-        <form className="form-container" onSubmit={handleSubmit}>
-          <select id="houses" name="houses" className="select_house mt-3" onChange={handleHouseChange} required>
-            <option value="">Select A House</option>
-            {
-                houses.length > 0 ? (
-                  houses.map((house) => (
-                    <Selection
-                      key={house.id}
-                      name={house.name}
-                      id={house.id}
-                    />
-                  ))
-                ) : (
-                  <option value="no house">No houses avaliable</option>
-                )
-              }
-          </select>
-          <DatePicker
-            className=""
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-            placeholderText="start date: dd/mm/yyyy"
-            dateFormat="dd/MM/yyyy"
-            required
-          />
+    <div className="reserve-bg">
+      <div className="d-none d-lg-block p-3">
+        <Link to="/home" className="text-decoration-none">
+          <AiFillHome style={{ color: 'white', fontSize: '36px' }} />
+        </Link>
+      </div>
+      <div className="contenedor col-lg-12 p-0">
+        <div className="content text-white">
+          <div className="header">
+            <h3 className="mb-4">RESERVE A HOUSE</h3>
+            <span className="line text-center mx-auto mb-4" />
+          </div>
+          <p className="description text-center">
+            Embark on a journey of unrivaled luxury as you step into
+            our extraordinary collection of rental homes.
+            Prepare to be captivated by a world of refinement and elegance,
+            where every aspect has been thoughtfully curated to
+            surpass even the loftiest of expectations.
+            Immerse yourself in awe-inspiring architectural masterpieces
+            and indulge in lavish amenities, as our opulent residences
+            offer a lifestyle that epitomizes pure extravagance.
+            Experience a new standard of sophistication and elevate your
+            living to extraordinary heights.
+          </p>
+          <form className="form-container text-center" onSubmit={handleSubmit}>
+            <select id="houses" name="houses" className="select_house2 text-center mb-4" onChange={handleHouseChange} required>
+              <option value="" className="text-dark">Select A House</option>
+              {
+                        houses.length > 0 ? (
+                          houses.map((house) => (
+                            <Selection
+                              className="text-dark"
+                              key={house.id}
+                              name={house.name}
+                              id={house.id}
+                            />
+                          ))
+                        ) : (
+                          <option value="no house">No houses avaliable</option>
+                        )
+                      }
+            </select>
+            <div className="justify-content-between mb-4">
+              <DatePicker
+                className="select_house text-center"
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                placeholderText="Start Date"
+                dateFormat="dd/MM/yyyy"
+                required
+              />
 
-          <DatePicker
-            className=""
-            selected={endDate}
-            onChange={(date) => setEndDate(date)}
-            placeholderText="end date: dd/mm/yyyy"
-            dateFormat="dd/MM/yyyy"
-            required
-          />
-          <button type="submit" className="mt-3">Reserve Now</button>
-        </form>
-        <div className="status-notification">
-          {status}
+              <DatePicker
+                className="select_house text-center"
+                selected={endDate}
+                onChange={(date) => setEndDate(date)}
+                placeholderText="End Date"
+                dateFormat="dd/MM/yyyy"
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-light rounded-pill mx-auto">Reserve Now</button>
+          </form>
+          <div className="status-notification">
+            {status}
+          </div>
         </div>
       </div>
     </div>
