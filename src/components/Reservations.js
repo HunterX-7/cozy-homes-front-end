b76a6c6e5e32172a44c9a47b5fd540c593921a37
gabicilfone <cilfonegabriel@gmail.com>
2023-05-23 17:31:43 -0300

    add images

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router';
 import { func } from 'prop-types';
 import { fetchReservation } from '../redux/reservationSlice';
 import ReservationsList from './ReservationsList';
+import reservationLogo from './images/reservation.png';
+
 
 const Reservations = (props) => {
   const { SessionStatus } = props;
@@ -31,9 +33,25 @@ const Reservations = (props) => {
       <div className="col-12">
         <div className="box" />
       </div>
+      <table className="reservation-table">
+            <thead>
+              <tr className="table-header">
+                <th colSpan="5">
+                  <img src={reservationLogo} alt="Reservation Logo" className='reservation-lg'/>
+                </th>
+              </tr>
+              <tr>
+                <th className="title-table">User name</th>
+                <th className="title-table">House Name</th>
+                <th className="title-table">Location</th>
+                <th className="title-table">Start Date</th>
+                <th className="title-table">End Date</th>
+              </tr>
+            </thead>
       <ReservationsList
         ReservationsData={reserves}
       />
+      </table>
     </div>
 
   );
